318f35d73cd6246a6f278d4b8c5d04b255607788
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-12-05 12:28:26 +0300

    craete superuser and listview class

--- insta/models.py
+++ insta/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     image = models.ImageField(blank = True, null = True)
     caption = models.CharField(max_length=200)
     date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.caption
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,12 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import (
+    ListView
+)
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    template_name = "home.html"
+    QuerySet = Post.objects.all()
+    context_object_name = 'post'
