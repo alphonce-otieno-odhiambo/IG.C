0bf8655e8c0e93cda921057cad32decff6d2b0d4
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-12-06 21:03:27 +0300

    create a comment template

--- insta/views.py
+++ insta/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # from django.views.generic import (
 #     ListView
 # )
+from .forms import CommentForm
 from .models import Post
 
 # # Create your views here.
@@ -15,3 +16,14 @@ def postview(request):
     posts = Post.objects.all()
     context = {"posts":posts}
     return render (request, 'home.html', context)
+
+def comment(request):
+    if request.method =="POST":
+        form = CommentForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return render('home')
+    else:
+        form = CommentForm()
+    context = {"form":form}
+    return render(request, 'comment.html', context)
