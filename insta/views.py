94d791498d0d5ea50c17ea85bd84adb320cd9ac3
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-12-08 01:13:20 +0300

    create a login view

--- insta/views.py
+++ insta/views.py
@@ -67,3 +67,15 @@ def register_view(request):
         return redirect('/login')
     context ={"form":form}
     return render(request, 'accounts/register.html', context)
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid:
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+            form = AuthenticationForm(request)
+    context = {"form":form}
+    return render(request, 'accounts/login.html',context)
